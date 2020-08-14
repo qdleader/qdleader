fe471435a1aacd26b4f750c0bfdd8a733e5311a2
qdleader <yk4545945@163.com>
2020-08-14 20:31:27 +0800

    分类列表

--- react/react-admin/react-admin/src/pages/admin/category/category.jsx
+++ react/react-admin/react-admin/src/pages/admin/category/category.jsx
@@ -4,23 +4,47 @@ import { Card ,Icon ,Button,Table,message} from 'antd';
 
 import {reqList} from '../../../api';
 
-// import LinkButton from '../../components/link-button/link-button'
+ import LinkButton from '../../../components/link-button/link-button'
 export default class Category extends Component {
 		state= {
 			categorys:[],// 分类列表
+			subCategorys:[],// 子分类列表
+			loading:true,//请求过程中列表加载
+			parentId:'0',//要穿的id
+			parentName:'',//显示的title
+		}
+		showSub = (data) => {
+			console.log("二级列表",data)
+			this.setState({
+				parentId:data.id
+			},() => {
+				console.log("parentId",this.state.parentId)
+				this.getListq()
+			})
+			
 		}
 		// 获取列表数据
 		getListq = async () => {
-			const res = await reqList('0')
+			this.setState({loading: true})
+			const {parentId} = this.state
+			const res = await reqList(parentId)
 			console.log("返回的结果",res)
+			this.setState({loading:false})
 			if(res.ret== 0) {
 				res.data.data.map(item => {
 					item.key = item.id
 				})
 				const categorys = res.data.data;
-				this.setState({
-					categorys,
-				})
+				if(parentId == 0) {
+					this.setState({
+						categorys,
+					})
+				} else {
+					this.setState({
+						subCategorys:categorys
+					})
+				}
+				
 			} else {
 				message.error('获取列表失败')
 			}
@@ -41,10 +65,10 @@ export default class Category extends Component {
 					{
 						title:'操作',
 						width:300,
-						render:() => (
+						render:(data) => (
 							<span>
-								<a href="#">修改分类</a>
-								<a href="#">查看子分类</a>
+								<LinkButton>修改分类</LinkButton>
+								<a href="#" onClick={() => {this.showSub(data)}}>查看子分类</a>
 							</span>
 						)
 					}
@@ -60,8 +84,8 @@ export default class Category extends Component {
 
 	
 	render() {
-				//读取状态数据
-		const {categorys} = this.state
+		//读取状态数据
+		const {categorys,subCategorys,loading,parentId} = this.state
 		const title = '一级分类列表'
 		const extra = (
 			<Button>
@@ -70,31 +94,15 @@ export default class Category extends Component {
 			</Button>
 		)
 		
-		const dataSource = [
-  {
-    key: '1',
-    name: '胡彦斌',
-    age: 32,
-    address: '西湖区湖底公园1号',
-  },
-  {
-    key: '2',
-    name: '胡彦祖',
-    age: 42,
-    address: '西湖区湖底公园1号',
-  },
-];
-
-
-
 	
 		return (
 
 			<div>
 				 <Card title={title} extra={extra}>
 					<Table bordered 
-						dataSource={categorys} 
+						dataSource={parentId == 0 ? categorys : subCategorys} 
 						columns={this.columns} 
+						loading={loading}
 						pagination={{defaultPageSize:5,showQuickJumper:true}}
 						/>
 				</Card>
