cc5f409b1723b24d76af401f67a0b079cd64eb81
前端架构师 <62106726+qdleader@users.noreply.github.com>
2022-06-04 21:03:39 +0800

    feat:ts的类型断言

---
+++ Typescript/ts的类型断言.ts
@@ -0,0 +1,11 @@
+let num: number | string = '10'
+
+// 第一种 （在tsx 中不能用这种）
+function getAs(name: string | number) {
+    return (<string>name).length
+}
+
+// 第二种
+function getAs1(name: string | number) {
+    return (name as string).length
+}
