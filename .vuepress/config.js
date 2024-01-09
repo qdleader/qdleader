731da7421a2a3a9976dfae5f30f094dc8101e1de
qdleader <yk4545945@163.com>
2024-01-09 11:30:56 +0800

    init docs

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -9,7 +9,7 @@ import { clipboardPlugin } from 'vuepress-plugin-clipboard'
 
 export default defineUserConfig({
 	lang: "zh-CN",
-	base: "/docs/",
+	base: "/qdleader/",
 	title: "你好， qdleader ！",
 	description: "qdleader,前端面试题大全, 编程学习路线, 编程知识百科, Java, 编程导航, 前端, 开发, 编程分享, 项目, IT, 求职, 面经",
 	// plugins: [
