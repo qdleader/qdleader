9a262f5c20b8d8197ccddc9b51cb712ad6b9b795
qdleader <yk4545945@163.com>
2024-07-08 13:39:31 +0800

    feat: meta 优化

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -40,6 +40,7 @@ export default defineUserConfig({
 				content: "qdleader,前端面试题大全, 编程学习路线, 编程知识百科, Java, 编程导航, 前端, 开发, 编程分享, 项目, IT, 求职, 面经",
 			},
 			['meta', {name: 'referrer', content: 'no-referrer-when-downgrade'}],
+			['meta', {name: 'google-site-verification', content: 'q56zlN4YQIQUwgGTM80BLZwESirvmrp0h9e7SHfg_Gg'}],
 		],
 		[
 			"script",
