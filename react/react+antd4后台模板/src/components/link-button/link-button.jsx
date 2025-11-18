71e2a2fc40038b13e9ca4b85aa0043e3d41a3933
qdleader <yk4545945@163.com>
2025-11-18 17:44:58 +0800

    refactor: remove unused files and components from the React project

--- react/react+antd4后台模板/src/components/link-button/link-button.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import './index.less'
-
-export default function LinkButton(props) {
-	return <button {...props} className="link-button"></button>
-}
--- react/react+antd4后台模板/src/pages/admin/pageNotFound/pageNotFound.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function pageNotFound() {
-    return (
-        <div>
-            <h1>404</h1>
-        </div>
-    )
-}
